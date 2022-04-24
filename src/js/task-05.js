e96aa7dc627c0343e225a678f0f06951aa3ebc92
Tatiana Yarchenko <ktanya61@gmail.com>
2022-04-24 16:27:07 +0300

    HW-6 done

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -9,6 +9,6 @@ refs.input.addEventListener("input", onInputChange);
 function onInputChange(event) {
   refs.span.textContent = event.currentTarget.value;
   if (event.currentTarget.value === "") {
-    refs.span.textContent === "Anonymous";
+    refs.span.textContent = "Anonymous";
   }
 }
